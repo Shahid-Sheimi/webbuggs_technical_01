fc7f8da2ae34e29442748dfacf702a98a397da3b
Shahid Hussain <74352379+Shahid-Sheimi@users.noreply.github.com>
2024-01-01 19:11:15 +0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
+# app/admin.py
 from django.contrib import admin
+from .models import ProductCategory, SubCategory, Color, Product
 
-# Register your models here.
+# Register the models directly
+admin.site.register(ProductCategory)
+admin.site.register(SubCategory)
+admin.site.register(Color)
+admin.site.register(Product)
