0ff4daf88220e305fe971efbb2185ed53952c95f
Shahid Hussain <74352379+Shahid-Sheimi@users.noreply.github.com>
2024-01-02 15:09:15 +0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from .views import (
-    
-    UserSignupView, UserLoginView, UserListView, UserDetailView, UserUpdateView, UserDeleteView,
-    
+    UserSignupView, UserLoginView, UserListView, UserDetailView, UserUpdateView, UserDeleteView,  
 )
-
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='user-signup'),
     path('login/', UserLoginView.as_view(), name='user-login'),
@@ -12,6 +9,4 @@ urlpatterns = [
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
-
-
 ]
