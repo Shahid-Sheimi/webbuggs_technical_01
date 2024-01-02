d6cf841a4ce55b7d30630d225af28b5ac20146a3
Shahid Hussain <74352379+Shahid-Sheimi@users.noreply.github.com>
2024-01-02 15:17:42 +0500

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-
-
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
