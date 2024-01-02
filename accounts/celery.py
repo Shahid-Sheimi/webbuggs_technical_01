4d6d5f589b405bcb4b1a182e3cc9c768439390e5
Shahid Hussain <74352379+Shahid-Sheimi@users.noreply.github.com>
2024-01-02 15:09:47 +0500

    Update celery.py

--- accounts/celery.py
+++ accounts/celery.py
@@ -1,18 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'technical_task.settings')
-
 # create a Celery instance and configure it using the settings from Django
 app = Celery('accounts')
-
 # Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 # Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
-
 if __name__ == '__main__':
     app.start()
