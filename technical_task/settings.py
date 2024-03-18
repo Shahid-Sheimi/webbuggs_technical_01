61e13ec1e1271637544e5db0b70953b527e233a2
Shahid Hussain <74352379+Shahid-Sheimi@users.noreply.github.com>
2024-03-19 03:09:26 +0500

    Update settings.py

--- technical_task/settings.py
+++ technical_task/settings.py
@@ -101,13 +101,7 @@ DATABASES = {
     }
 }
 
-# settings.py
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shahidhosseinoff@gmail.com'
-EMAIL_HOST_PASSWORD = 'okru qkix uwos idzp '
+
 
 # Password validation
 
